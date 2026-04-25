3544cde26ce0a5a59064735d43be1fecfde974b8
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-04-25 18:00:21 -0300

    feat(javascript): adiciona tipos primitivos, declaracao de variaves, expressoes e operadores com javascript

---
+++ csshortcut-app/javaScript/index.js
@@ -0,0 +1,54 @@
+// comentários sao escritos assim para uma linha
+/**
+ * assim para mais de uma linha
+ */
+
+/**
+ * PARTE 1: Tipos primitivos
+ * https://developer.mozilla.org/pt-BR/docs/Glossary/Primitive
+ */
+
+// string
+'Meu nome é'
+
+// number
+42
+
+// boolean
+true
+false
+
+/**
+ * Null undefined symbol
+ */
+
+/**
+ * PARTE 2: Declarando variaveis (variables)
+ * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Grammar_and_types
+ */
+var nome = 'Paulinha';
+
+nome = 'Paola bracho'
+
+console.log(nome);
+
+/**
+ * PARTE 3: Operadores
+ * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
+ */
+var result = 42;
+
+var result1 = 42 + 10;
+
+var result2 = 42 - 10;
+
+var result3 = 42 * 10;
+
+var result4 = (42 / 10) * 1 - 3;
+
+var nome = 'Paula';
+var sobrenome = 'Figueiredo'
+
+var concatenando = sobrenome + ', ' + nome; 
+
+console.log(concatenando);
