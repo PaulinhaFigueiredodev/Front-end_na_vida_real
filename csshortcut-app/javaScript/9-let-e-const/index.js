0f378c271853c250741b529dc99cda94363fd9bb
Paula Figueiredo <paulafigueiredo36@gmail.com>
2026-08-02 23:30:12 -0300

    feat: adiciona aula sobre let e const

---
+++ csshortcut-app/javaScript/9-let-e-const/index.js
@@ -0,0 +1,13 @@
+// O let permite declarar variáveis que podem ter seus valores alterados posteriormente. Por exemplo:
+let idade = 25;
+idade = 30;
+
+console.log(idade); // Saída: 30
+
+// O const permite declarar variáveis que não podem ter seus valores alterados posteriormente. Por exemplo:
+const nome = 'Paula';
+
+// Isso causará um erro, pois não é possível alterar o valor de uma variável declarada com const.
+// Para visualizar o erro, você pode descomentar a linha abaixo:
+// nome = 'Figueiredo'; 
+console.log(nome);
